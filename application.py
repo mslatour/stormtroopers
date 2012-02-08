e83a79764cefde81001fcf19f4b3f76564ab744c
Sander Latour <latour@uva.nl>
2012-02-08 16:45:47 +0100

    Created run modes

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
 import domination.run
+import sys
 
 class MyScenario(domination.run.Scenario):
   EPISODES = 10
 
-MyScenario.test('trooper.py', 'domination/agent.py')
+def run(mode):
+  if mode == "sander":
+    MyScenario.test('trooper_sander.py', 'domination/agent.py')
+  elif mode == "frank":
+    MyScenario.test('trooper_frank.py', 'domination/agent.py')
+  elif mode == "daniel":
+    MyScenario.test('trooper_daniel.py', 'domination/agent.py')
+  elif mode == "sicco":
+    MyScenario.test('trooper_sicco.py', 'domination/agent.py')
+  else:
+    print "Usage: python application.py <mode>"
+
+if __name__ == "__main__":
+  if len(sys.argv) == 1:
+    print "Usage: python application.py <mode>"
+  else:
+    run(sys.argv[1])
